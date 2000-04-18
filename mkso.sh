dbdbd4fb43597c28247366b2ba17357f26d3459d
D. Richard Hipp <drh@acm.org>
2000-04-19 00:41:00 +0200

    generates pkgIndex.tcl (CVS 141)

--- mkso.sh
+++ mkso.sh
@@ -1,19 +1,19 @@
 #! /bin/sh
 #
-# This script builds "libtkhtml.so" for Linux and Tcl/Tk8.1.1-stubs.
+# This script builds "libtkhtml.so" for Linux and Tcl/Tk8.3-stubs.
 # First do "make srcdir; cd srcdir".  Then run this script.
 #
-# $Revision: 1.5 $
+# $Revision: 1.6 $
 #
 
-TCLBASE=/home/drh/tcltk/tcl8.3.0
-TKBASE=/home/drh/tcltk/tk8.3.0
+BASE=/home/drh/tcltk/8.3linux
+VERS=8.3
 
-TKLIB="/home/drh/tcltk/linux/lib/libtkstub8.3g.a -L/usr/X11R6/lib -lX11"
-TCLLIB="/home/drh/tcltk/linux/lib/libtclstub8.3g.a -lm -ldl"
+TKLIB="$BASE/libtkstub${VERS}g.a -L/usr/X11R6/lib -lX11"
+TCLLIB="$BASE/libtclstub${VERS}g.a -lm -ldl"
 
 CC='gcc -fPIC -DUSE_TCL_STUBS=1 -DUSE_TK_STUBS=1 -O2'
-INC="-I. -I$TCLBASE/generic -I$TKBASE/generic"
+INC="-I. -I$BASE"
 
 CMD="rm *.o"
 echo $CMD
