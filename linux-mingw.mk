753a033e4286600e0ca4d9f7f204515fd79cf7d2
D. Richard Hipp <drh@acm.org>
2002-12-17 21:13:00 +0100

    Add the makefile for the alternative mingw compilation. (CVS 177)
    ADDED   linux-mingw.mk

---
+++ linux-mingw.mk
@@ -0,0 +1,49 @@
+#!/usr/bin/make
+#
+#### The toplevel directory of the source tree.
+#
+SRCDIR = /home/drh/tkhtml/htmlwidget
+
+#### C Compiler and options for use in building executables that
+#    will run on the platform that is doing the build.
+#
+BCC = gcc -g -O2
+
+#### Name of the generated static library file
+#
+LIBNAME = libtkhtml.a
+
+#### The suffix to add to executable files.  ".exe" for windows.
+#    Nothing for unix.
+#
+E =
+
+#### C Compile and options for use in building executables that 
+#    will run on the target platform.  This is usually the same
+#    as BCC, unless you are cross-compiling.
+#
+#TCC = gcc -O6
+#TCC = gcc -g -O0 -Wall
+#TCC = gcc -g -O0 -Wall -fprofile-arcs -ftest-coverage
+TCC = /opt/mingw/bin/i386-mingw32msvc-gcc -O6 -DSTATIC_BUILD=1
+
+#### Include file directories for Tcl and Tk.
+#
+INC = -I/home/drh/tcltk/8.4win
+
+#### Extra arguments for linking 
+#
+LIBS = 
+
+#### Command used to build a static library
+#
+AR = /opt/mingw/bin/i386-mingw32msvc-ar r
+RANLIB = /opt/mingw/bin/i386-mingw32msvc-ranlib
+
+#### The TCLSH command
+#
+TCLSH = tclsh
+
+# You should not need to change anything below this line
+###############################################################################
+include $(SRCDIR)/main.mk
