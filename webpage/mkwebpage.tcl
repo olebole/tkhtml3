9b9c527ea813484591d01d9e60e6306d24ae766c
Daniel Kennedy <danielk1977@gmail.com>
2005-03-11 14:03:00 +0100

    Update to webpage. (CVS 191)

--- webpage/mkwebpage.tcl
+++ webpage/mkwebpage.tcl
@@ -2,204 +2,245 @@
 #
 # Construct the web page for tkhtml
 #
-# @(#) $Id: mkwebpage.tcl,v 1.9 2002/09/22 17:03:00 peter Exp $
+# @(#) $Id: mkwebpage.tcl,v 1.10 2005/03/11 14:03:31 danielk1977 Exp $
 #
 
-set p [open publish.sh w]
-puts $p "#!/bin/sh"
-puts $p "#"
-
-set SendList {}
-
-set f [open index.html w]
-puts $f {
-<html>
-<head>
-<title>An HTML Widget for Tcl/Tk</title>
-</head>
-<body bgcolor=white>
-<h1 align=center>An HTML Widget For Tcl/Tk</h1>
+proc header {} {
+  puts {
+    <html>
+    <head>
+    <title>An HTML Widget For Tk</title>
+    </head>
+    <body>
+  }
 }
-puts $f "<p align=center><i>Last update: [clock format [clock seconds]]</i></p>"
-puts $f {
-<p>"Tkhtml" is a Tcl/Tk widget that displays HTML.  Tkhtml
-is implemented in C.  It is a true widget, not a metawidget implemented
-using the Text or Canvas widgets of the Tcl/Tk core.  Implementing
-Tkhtml in C gives it a number of advantages:</p>
 
-<p>
+proc footer {} {
+  puts {
+    </body>
+    </html>
+  }
+}
+
+set ::TITLE {}   ;# The title
+set ::LINKS {}   ;# Links are added to this variable.
+set ::BODY {}    ;# Body of html document is built up in this variable
+
+proc p {text} {
+  append ::BODY <p>
+  append ::BODY $text
+  append ::BODY </p>
+}
+
+proc link {caption href} {
+  append ::LINKS "<li><a href=\"#$href\">$caption</a></li>"
+}
+
+set ::H 0
+proc h {level text} {
+  set var ::BODY
+  if {$level==1} {
+    set var ::TITLE
+  }
+  append $var <h$level>
+  if {$level==2} {
+    set name "part[incr ::H]"
+    append ::BODY "<a name=\"$name\"></a>"
+    link $text $name
+  }
+  append $var $text
+  append $var </h$level>
+}
+
+proc output_page {} {
+  header
+  puts $::TITLE
+  puts {<div id="links"><ul>}
+  puts $::LINKS
+  puts {</ul></div>}
+  puts $::BODY
+  footer
+}
+###########################################################################
+# Document content is below this line.
+
+h 1 {An HTML Widget For Tcl/Tk}
+append ::TITLE "<p><i>Last update: [clock format [clock seconds]]</i></p>"
+
+p {
+  "Tkhtml" is a Tcl/Tk widget that displays HTML. Tkhtml is implemented in C.
+  It is a true widget, not a metawidget implemented using the Text or Canvas
+  widgets of the Tcl/Tk core. The current version of Tkhtml 2.0, which has
+  not changed significantly for several years. This page houses that version
+  and is also home to the (currently embryonic) plans for a revitalisation.
+}
+
+p {
+  Tkhtml was created by D. Richard Hipp, and has since been enhanced by 
+  Peter MacDonald.
+}
+
+h 2 {Current Status (Version 2.0)}
+
+p {
+  The current version of Tkhtml is more or less compatible with the HTML 3.2
+  standard. The majority of documents on the web today are rendered 
+  acceptably, however results are sometimes less attractive than when using 
+  a more modern rendering engine, like Gecko or KHTML. This is particularly
+  true when stylesheets are in use. Having said that, some advantages of
+  Tkhtml over other similar widgets are:
 <ul>
-<li> It runs fast and uses little memory.</li>
-<li> It supports smooth scrolling.</li>
-<li> It supports text wrap-around on images and tables.</li>
-<li> It has a full implementation of tables. Complex pages (such as 
+<li>It runs very fast and uses little memory.</li>
+<li>It supports smooth scrolling.</li>
+<li>It supports text wrap-around on images and tables.</li>
+<li>It has a full implementation of tables. Complex pages (such as 
      <a href="http://www.scriptics.com/">http://www.scriptics.com/</a>)
      are displayed correctly.</li>
-<li> Supports forms. </li>
-<li> It supports the &lt;APPLET&gt;, &lt;SCRIPT&gt; and &lt;EMBED&gt;. 
-     (Partially.  Full support is pending.) </li>
-<li> Support for frames is planned. </li>
+<li>It has an API that allows applications to provide configurable support 
+     for applets, scripts and forms via callbacks.
 </ul>
-</p>
+}
 
-<p>Tkhtml can be used with Tcl/Tk8.0 or later.
-The shared libraries use the new stubs mechanism, so you
-should be able to load Tkhtml with any version of "wish" beginning
-with 8.0.6.</p>
+p { 
+  Tkhtml 2.0 can be used with Tcl/Tk8.0 or later. Shared libraries use 
+  the TCL stubs mechanism, so you should be able to load Tkhtml with 
+  any version of "wish" beginning with 8.0.6.
+}
 
-<p>At the moment, there is not a lot of software that uses this
-widget.  Tkhtml is not an application in and of itself.  It is only
-a tool.  But applications are being built around tkhtml.  Check back
-later for new developments.</p>
+h 2 {Revitalisation Effort}
 
-<h2>Mailing List!</h2>
-
-<p><font color=red><b><i>New!</i></b></font>
-   A mailing list has been started for users of tkhtml.
-   Sign up if you want to recieve notifications of updates or
-   ask questions about using tkhtml.</p>
-
-<form method=GET action="http://www.egroups.com/subscribe">
-<input type=hidden name="listname" value="tkhtml">
-<input type=hidden name="SubmitAction" value="Subscribe">
-<p>Enter your e-mail address below and click the button to
-   sign up for the tkhtml mailing list:</p>
-
-<p>
-<table cellspacing=10>
-<tr><td valign=center>
-<input type=text name="emailaddr" value="your e-mail" size=21>
-</td><td valign=center>
-<input type=image border=0 alt="Click here to join tkhtml"
-   name="Click here to join tkhtml" SRC="http://www.egroups.com/oems/default/languages/english/images/subscriptionBoxButton.gif">
-</td></tr>
-</table>
-</p>
-
-<p>You can also view the 
-  <a href="http://www.egroups.com/group/tkhtml/">archives</a>
-  for the mailing list.  To post to this mailing list, send a
-  message to <a href="mailto:tkhtml@eGroups.com">tkhtml@eGroups.com</a>.
-  The mailing list is hosted by
-  <a href="http://www.egroups.com/">eGroups.com</A>
-</p>
-</form>
-
-
-<h2>You Can Help!</h2>
-
-<p>If you would like to help, please consider
-contributing in the following ways:</p>
-
-<p>
+p { 
+  The current plan is to upgrade and modernise Tkhtml over the coming 
+  months. Some of the main goals of this are:
 <ul>
-<li> Try out tkhtml on your computer and report bugs to
-     <a href="mailto:drh@acm.org">drh@acm.org</a>. </li>
-<li> Fix bugs and send in patches.  (Write access to the
-     CVS repository may be granted to anyone who is serious
-     about this.)</li>
-<li> Make suggestions for new features.  </li>
-<li> Write applications that use tkhtml. </li>
-<li> Improve the documentation. </li>
+<li>Support (where applicable) for the HTML 4.01, XHTML 1.1 and CSS 2.1 
+    standards.</li>
+<li>To provide interfaces functionally equivalent to the W3C DOM interfaces
+    (so that a full DOM implementation could be built on top of them).</li>
+<li>Modernisation of the internals of the widget to take advantage of 
+    Tcl and Tk APIs introduced since the current code was written. </li>
+<li>Eventual inclusion in the Tk core.</li>
 </ul>
-</p>
-
-<p>Any help you can provide is appreciated.</p>
-
-<h2>Getting The Widget</h2>
-
-<p>Visit the <a href="download.html">download</a> page for a list of
-files available for immediate download.</p>
-
-<p>You can now also obtain the latest tkhtml sources via anonymous CVS.
-To access the anonymous CVS server, first install CVS on your system.
-(See <a href="http://www.cyclic.com/">http://www.cyclic.com/</a> for 
-additional information.)  Then login as follows:</p>
-
-<blockquote><pre>
-cvs -d :pserver:anonymous@cvs.hwaci.com:/home/cvs/tkhtml login
-</pre></blockquote>
-
-<p>You will be prompted for a password.  Use "<tt>anonymous</tt>".  After
-you get logged in successfully, you can check out the source tree
-like this:</p>
-
-<blockquote><pre>
-cvs -d :pserver:anonymous@cvs.hwaci.com:/home/cvs/tkhtml checkout htmlwidget
-</pre></blockquote>
-
-<p>This command creates a directory named "<tt>htmlwidget</tt>" and
-fills it with the latest version of the sources.</p>
-
-<p>You can view a log of changes, create trouble tickets, or read or
-enter Wiki about TkHTML by visiting the CVSTrac server at</p>
-
-<blockquote>
-<a href="http://cvs.hwaci.com:2080/tkhtml/">
-http://cvs.hwaci.com:2080/tkhtml/</a>
-</blockquote>
-
-<p>Note that the cvs.hwaci.com machine is connected to the internet
-via a cable modem which changes IP addresses 3 or 4 times a year.
-If you have trouble getting through, it may be because there was a
-recent IP address change and the new address has not yet had time
-to propagate through DNS.  Try again in a few days.</p>
-
-
-</body>
-</html>
 }
-close $f
-lappend SendList index.html
 
-set f [open download.html w]
-puts $f {
-<html>
-<head>
-<title>TkHtml Download Page</title>
-</head>
-<body bgcolor=white>
-<h1 align=center>TkHtml Download Page</h1>
+p {
+  More detail is available in the <a href="requirements.html">
+  Tkhtml revitalization requirements draft</a>.
 }
-puts $f "<p align=center><i>Last update: [clock format [clock seconds]]</i></p>"
-puts $f {
 
-<p>The files shown below are available for download.  For the very
-latest sources, visit the anonymous CVS server.  Instructions for 
-reaching the anonymous CVS server are on the tkhtml 
-<a href="index.html">homepage</a>.</p>
-
+p {
+  Being an open-source project, if you have some time to spare you can help!
+  For example, if you wanted to you could volunteer to:
 <ul>
+<li>Assist with programming the C code for the widget.</li>
+<li>Write a TCL application to help test the code as it evolves.</li>
+<li>Help with designing and reviewing the new interface.</li>
+<li>Improve internal or external documentation.</li>
+<li>Create a better design for this webpage</li>
+<li>Help with the interpretation of the HTML/CSS specifications (it would 
+    be great to have someone who knows these well to act as a consultant,
+    even if your time is very limited).</li>
+</ul>
 }
+
+p {
+  Any help you can provide will be gratefully received. Send email to 
+  one of the contacts below if you are interested. Don't be shy!
+}
+
+h 2 {Contacts/Support}
+
+p {
+  You can view a log of changes, create trouble tickets, or read or
+  enter Wiki about TkHTML by visiting the CVSTrac server at:
+  <blockquote>
+  <a href="http://tkhtml.tcl.tk/cvstrac">http://tkhtml.tcl.tk/cvstrac</a> 
+  </blockquote>
+}
+
+p {
+  There is a mailing list hosted by Yahoo groups. You can sign up
+  or review the archive at:
+  <blockquote>
+    <a href="http://groups.yahoo.com/group/tkhtml/">
+    http://groups.yahoo.com/group/tkhtml/</a>
+  </blockquote>
+}
+
+p {
+  If you want to help, then you can send mail to one of the following 
+  contacts (who are both subscribed to and read the mailing list).
+  <blockquote>
+  <a href="mailto:danielk1977@yahoo.com">danielk1977@yahoo.com</a> (Dan)
+  <br>
+  <a href="mailto:drh@hwaci.com">drh@hwaci.com</a> (Richard)
+  </blockquote>
+}
+
+h 2 {Source Code}
+
+p {
+  To obtain the source code for Tkhtml, use anonymous CVS (See 
+  <a href="http://www.cyclic.com/">http://www.cyclic.com/</a> for 
+  additional information on CVS.) Login as follows:
+}
+
+p {
+  <blockquote><pre>
+cvs -d :pserver:anonymous@tkhtml.tcl.tk:/tkhtml login
+  </pre></blockquote>
+}
+
+p {
+  You will be prompted for a password.  Use "<tt>anonymous</tt>".  After
+  you get logged in successfully, you can check out the source tree
+  like this:
+}
+
+p {
+  <blockquote><pre>
+cvs -d :pserver:anonymous@tkhtml.tcl.tk:/tkhtml checkout htmlwidget
+  </pre></blockquote>
+}
+
+p {
+  This command creates a directory named "<tt>htmlwidget</tt>" and
+  fills it with the latest version of the sources.
+}
+
+h 2 {Binaries}
+
+p {
+  The following binaries are available. To quickly test the capabilities of 
+  the widget on Linux or Windows x86, download and run one of the executable 
+  versions of the "Html Viewer" app.  The viewer app comes with a few 
+  built-in documents, or can load a document from the local file-system.
+}
+
+append ::BODY <ul>
 foreach {file desc} {
   tkhtml.tar.gz   {A tarball containing all the latest source code}
   hv.tcl          {The "Html Viewer" example application}
   spec.html       {A raw specification of how the tkhtml widget works}
   tkhtml.so       {Shared library suitable for use on Linux}
   tkhtml.dll      {A DLL suitable for use on Windows95/98/NT/2K}
+  hv.exe           {Executable version of the "Html Viewer" app for Windows}
+  hv.linux-x86-xft {Executable version of the "Html Viewer" app for Linux}
 } {
   if {![file readable $file]} continue
   lappend SendList $file
-  puts $f "<li><p><a href=\"$file\">$file</a><br>"
-  puts $f "Description: $desc<br>"
-  puts $f "Size: [file size $file] bytes<br>"
-  puts $f "Last modified: [clock format [file mtime $file]]"
-  if {![catch {exec ident $file | grep {$Id: }} ident]} {
-    puts $f "<br>Version information:"
-    puts $f "<pre>\n$ident</pre>"
+  append ::BODY "<li><p><a href=\"$file\">$file</a><br>"
+  append ::BODY "Description: $desc<br>"
+  append ::BODY "Size: [file size $file] bytes<br>"
+  append ::BODY "Last modified: [clock format [file mtime $file]]"
+  if {![catch {exec strings $file | grep {$Id: }} ident]} {
+    append ::BODY "<br>Version information:"
+    append ::BODY "<pre>\n$ident</pre>"
   }
-  puts $f "</p></li>\n"
+  append ::BODY "</p></li>\n"
 }
-puts $f {
-</ul>
+append ::BODY </ul>
 
-<p><a href="index.html">Back</a> to the tkhtml home page</p>
+###########################################################################
+output_page
 
-</body>
-</html>
-}
-close $f
-lappend SendList download.html
-
-puts $p "scp [lsort $SendList] hwaci@oak.he.net:public_html/sw/tkhtml"
-close $p
