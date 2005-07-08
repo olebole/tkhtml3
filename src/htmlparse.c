1f37a2f0d410bdd6774b1cca3abb4644c1aa1e65
Daniel Kennedy <danielk1977@gmail.com>
2005-07-08 10:09:00 +0200

    Add BSD license text to various files. (CVS 293)

--- src/htmlparse.c
+++ src/htmlparse.c
@@ -1,24 +1,44 @@
-static char const rcsid[] =
-        "@(#) $Id: htmlparse.c,v 1.43 2005/07/06 10:42:02 danielk1977 Exp $";
-
 /*
-** A tokenizer that converts raw HTML into a linked list of HTML elements.
-**
-** This source code is released into the public domain by the author,
-** D. Richard Hipp, on 2002 December 17.  Instead of a license, here
-** is a blessing:
-**
-**    May you do good and not evil.
-**    May you find forgiveness for yourself and forgive others.
-**    May you share freely, never taking more than you give.
-*/
+ *--------------------------------------------------------------------------
+ * Copyright (c) 2005 Eolas Technologies Inc.
+ * All rights reserved.
+ *
+ * This Open Source project was made possible through the financial support
+ * of Eolas Technologies Inc.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the <ORGANIZATION> nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+static char const rcsid[] =
+        "@(#) $Id: htmlparse.c,v 1.44 2005/07/08 10:09:47 danielk1977 Exp $";
+
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
 #include <assert.h>
 #include "html.h"
-
 
 /*
  * The following elements have optional opening and closing types:
