fbba037ada2a3e9725dbff093fc6f8343ebd892f
Daniel Kennedy <danielk1977@gmail.com>
2006-02-25 13:29:00 +0100

    Fix for hv3 starkit on windows platform (CVS 398)

--- hv/main.tcl
+++ hv/main.tcl
@@ -2,7 +2,7 @@
 package require starkit
 starkit::startup
 if {[llength $::argv] == 0} {
-  set ::argv [list [file join [pwd] [file dirname [info script]] index.html]]
+  set ::argv [list [file join [file dirname [info script]] index.html]]
 }
 source [file join [file dirname [info script]] hv3_main.tcl] 
 
